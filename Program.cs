f2425c6c657eb2c446508bbfb5e8629edec7ac72
Élő Zalán <90603993+EloZalan@users.noreply.github.com>
2024-01-29 22:13:39 +0100

    Branch push teszt

--- Program.cs
+++ Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Most van egy új branchem :)");
+        Console.WriteLine(5);
+        Console.WriteLine("halo");
     }
 }
